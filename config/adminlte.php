acbf731d2d2e9bca885b20b823046eed62d586b4
romulo.melo <romulo.melopinheiro@gmail.com>
2021-09-14 18:38:57 -0400

    adicionado traducao pt-br

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => 'Igreja Digital',
+    'logo' => 'Bemol Digital',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Logo Igreja Digital',
+    'logo_img_alt' => 'Logo Bemol Digital',
 
     /*
     |--------------------------------------------------------------------------
